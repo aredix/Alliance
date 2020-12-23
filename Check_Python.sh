f06d0352815f36fd6e55717e946b9e3348bca091
Aredix <sokinggrey@gmail.com>
2020-12-23 09:25:48 +0100

    Starting main way to find bugs

---
+++ Check_Python.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ce script a pour but d'être lancé en amont de tout dépannage afin de vérifier la présence de python sur le system."
+echo "Prenez en compte que ce script est adapté pour être lancé sur un machine UNIX et non Windows."
+echo "La compatibilité MacOS n'est pas assurée."
+
+
