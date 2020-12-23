dd16af90fdece734662deddb71f11655f8a931a5
Aredix <sokinggrey@gmail.com>
2020-12-23 09:12:46 +0100

    First Pull

---
+++ scanErreurs.py
@@ -0,0 +1,7 @@
+import os
+operatingSystem = os.name
+
+print('Ce script a pour but de vérifier à un niveau basiques les erreus potentiels sur un autocom asterisk')
+print('')
+print("Operating System :", operatingSystem) 
+
