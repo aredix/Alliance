b2edbf43e05651df7b16f4d12ff05e81a5a92454
David Desplanque <32569871+aredix@users.noreply.github.com>
2021-01-11 16:56:16 +0100

    Delete Check_Python.sh

--- Suivis_appel/Check_Python.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Ce script a pour but d'etre lance en amont de tout depannage afin de verifier la presence de python sur le systeme."
-echo "Prenez en compte que ce script est adapte pour etre lance sur un machine UNIX et non Windows."
-echo "La compatibilité MacOS n'est pas assuree."
-
-
