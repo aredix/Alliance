7dd9a0cb909e6aca9861ba243c2d50ec3e766cc4
David Desplanque <32569871+aredix@users.noreply.github.com>
2021-01-11 16:56:26 +0100

    Delete scanErreurs.py

--- Suivis_appel/scanErreurs.py
+++
@@ -1,7 +0,0 @@
-import os
-operatingSystem = os.name
-
-print('Ce script a pour but de vérifier à un niveau basiques les erreus potentiels sur un autocom asterisk')
-print('')
-print("Operating System :", operatingSystem) 
-
